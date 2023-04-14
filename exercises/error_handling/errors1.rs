f651d53516c23602ddd412163bbe673610d781e8
Zhu <zjh815010623@gmail.com>
2023-04-15 00:51:12 +0800

    fix error1 & clippy2

--- exercises/error_handling/errors1.rs
+++ exercises/error_handling/errors1.rs
@@ -11,7 +11,7 @@ use std::fmt::format;
 pub fn generate_nametag_text(name: String) -> Result<String, String> {
     if name.is_empty() {
         // Empty names aren't allowed.
-        Err(format!("`name` was empty; it must be nonempty."))
+        Err("`name` was empty; it must be nonempty.".into())
     } else {
         Ok(format!("Hi! My name is {name}"))
     }
